fe7ed70883c022a98c52e0546d1be0cac399bec4
mariamm112 <mariamhilmi01@gmail.com>
2026-04-28 10:17:58 +0100

    appointments and services and login working

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,8 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AppointmentController;
+use App\Http\Controllers\ServiceController;
 Route::get('/', function () {
     return view('welcome');
 });
@@ -15,6 +16,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+     Route::get('/appointments/search', [AppointmentController::class, 'search'])
+        ->name('appointments.search');
 });
 
 require __DIR__.'/auth.php';
+Route::resource('appointments', AppointmentController::class)->middleware('auth');
+Route::resource('services', ServiceController::class)->middleware('auth');
